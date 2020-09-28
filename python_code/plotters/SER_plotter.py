b91bbf3addd9b574608d9d21011d4129f1937b83
tomer raviv <tomerraviv95@gmail.com>
2020-09-28 15:13:25 +0300

    Modified SER plotter

--- python_code/plotters/SER_plotter.py
+++ python_code/plotters/SER_plotter.py
@@ -1,10 +1,15 @@
+import math
+
+from python_code.utils.python_utils import load_pkl, save_pkl
+from python_code.trainers.VA.va_trainer import VATrainer
+from python_code.trainers.trainer import Trainer
+from dir_definitions import FIGURES_DIR, PLOTS_DIR
 import datetime
 import os
-
-from python_code.trainers.VA.va_trainer import VATrainer
-from dir_definitions import FIGURES_DIR
+from typing import List, Tuple
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np
 
 mpl.rcParams['xtick.labelsize'] = 16
 mpl.rcParams['ytick.labelsize'] = 16
@@ -24,26 +29,67 @@ MARKERS_DICT = {'Viterbi, CSI uncertainty': 'x',
 COLORS_DICT = {'Viterbi, CSI uncertainty': 'black',
                'Viterbi, perfect CSI': 'blue'}  # google green
 
-dec = VATrainer()
-ber_total, fer_total = dec.evaluate()
-snr_range = dec.snr_range['val']
-method_name = dec.get_name()
 
-current_day_time = datetime.datetime.now()
-folder_name = f'{current_day_time.month}-{current_day_time.day}-{current_day_time.hour}-{current_day_time.minute}_{dec.block_length}'
-if not os.path.isdir(os.path.join(FIGURES_DIR, folder_name)):
-    os.makedirs(os.path.join(FIGURES_DIR, folder_name))
+def get_ber_plot(dec: Trainer, run_over: bool):
+    method_name = dec.get_name()
 
-plt.figure()
-plt.plot(snr_range, ber_total, label=method_name, marker=MARKERS_DICT[method_name], color=COLORS_DICT[method_name],
-         linewidth=2.2, markersize=12)
-plt.yscale('log')
-plt.ylabel('SER')
-plt.xlabel('$E_b/N_0$ [dB]')
-plt.grid(which='both', ls='--')
-plt.xlim([snr_range[0] - 0.1, snr_range[-1] + 0.1])
-plt.legend(loc='lower left', prop={'size': 15})
+    # set the path to saved or needed-loading pkl file
+    if not os.path.exists(PLOTS_DIR):
+        os.makedirs(PLOTS_DIR)
+    file_name = '_'.join([method_name, str(dec.channel_type)])
+    plots_path = os.path.join(PLOTS_DIR, file_name + '.pkl')
 
-plt.savefig(os.path.join(FIGURES_DIR, folder_name, 'SER.png'), bbox_inches='tight')
+    if os.path.isfile(plots_path) and not run_over:
+        print("Loading plots")
+        ber_total = load_pkl(plots_path)
+    else:
+        print("calculating fresh")
+        ber_total, fer_total = dec.evaluate()
+        save_pkl(plots_path, ber_total)
 
-plt.show()
+    return ber_total
+
+
+def plot_all_curves(all_curves: List[Tuple[np.ndarray, np.ndarray, str]]):
+    current_day_time = datetime.datetime.now()
+    folder_name = f'{current_day_time.month}-{current_day_time.day}-{current_day_time.hour}-{current_day_time.minute}'
+    if not os.path.isdir(os.path.join(FIGURES_DIR, folder_name)):
+        os.makedirs(os.path.join(FIGURES_DIR, folder_name))
+
+    plt.figure()
+    min_snr = math.inf
+    max_snr = -math.inf
+    for snr_range, ber, method_name in all_curves:
+        plt.plot(snr_range, ber, label=method_name, marker=MARKERS_DICT[method_name], color=COLORS_DICT[method_name],
+                 linewidth=2.2, markersize=12)
+        min_snr = snr_range[0] if snr_range[0] < min_snr else min_snr
+        max_snr = snr_range[-1] if snr_range[-1] > max_snr else max_snr
+
+    plt.yscale('log')
+    plt.ylabel('SER')
+    plt.xlabel('$E_b/N_0$ [dB]')
+    plt.grid(which='both', ls='--')
+    plt.xlim([min_snr - 0.1, max_snr + 0.1])
+    plt.legend(loc='lower left', prop={'size': 15})
+    plt.savefig(os.path.join(FIGURES_DIR, folder_name, 'SER.png'), bbox_inches='tight')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    run_over = False
+    all_curves = []
+
+    # perfect CSI
+    dec1 = VATrainer(val_SNR_start=-6, val_SNR_end=10, noisy_est_var=0, gamma_start=0.1, gamma_end=2,
+                     gamma_num=20, channel_type='ISI_AWGN')
+    ber1 = get_ber_plot(dec1, run_over=run_over)
+    all_curves.append((dec1.snr_range['val'], ber1, dec1.get_name()))
+
+    # noisy estimate of CSI
+    dec2 = VATrainer(val_SNR_start=-6, val_SNR_end=10, noisy_est_var=0.1, gamma_start=0.1, gamma_end=2,
+                     gamma_num=20, channel_type='ISI_AWGN')
+    ber2 = get_ber_plot(dec2, run_over=run_over)
+    all_curves.append((dec2.snr_range['val'], ber2, dec2.get_name()))
+
+    plot_all_curves(all_curves)
