9ef75a0330eaa1b9444a1433d494f05a4e5217fb
tomer raviv <tomerraviv95@gmail.com>
2021-01-16 17:30:55 +0200

    fading_in_decoder simplification

--- python_code/channel/channel_estimation.py
+++ python_code/channel/channel_estimation.py
@@ -5,6 +5,8 @@ import numpy as np
 import scipy.io
 import os
 
+COST_LENGTH = 300
+
 
 def estimate_channel(memory_length: int, gamma: float, channel_coefficients: str, noisy_est_var: float = 0,
                      fading: bool = False, index: int = 0, fading_taps_type: int = 1):
@@ -21,7 +23,7 @@ def estimate_channel(memory_length: int, gamma: float, channel_coefficients: str
     if channel_coefficients == 'time_decay':
         h = np.reshape(np.exp(-gamma * np.arange(memory_length)), [1, memory_length])
     elif channel_coefficients == 'cost2100':
-        total_h = np.empty([500, memory_length])
+        total_h = np.empty([COST_LENGTH, memory_length])
         for i in range(memory_length):
             total_h[:, i] = scipy.io.loadmat(os.path.join(COST2100_DIR, f'combined_h_{i}'))[
                 'h_channel_response_mag'].reshape(-1)
@@ -34,7 +36,7 @@ def estimate_channel(memory_length: int, gamma: float, channel_coefficients: str
     if noisy_est_var > 0:
         h[:, 1:] += np.random.normal(0, noisy_est_var ** 0.5, [1, memory_length - 1])
     # fading in channel taps
-    if fading:
+    if fading and channel_coefficients == 'time_decay':
         if fading_taps_type == 1:
             fading_taps = np.array([51, 39, 33, 21])
             h *= (0.8 + 0.2 * np.cos(2 * np.pi * index / fading_taps)).reshape(1, memory_length)
@@ -54,12 +56,12 @@ if __name__ == '__main__':
     channel_coefficients = 'cost2100'  # 'time_decay','cost2100'
     fading_taps_type = 1
     fading = False
-    channel_length = 300
+    channel_length = COST_LENGTH
 
     total_h = np.empty([channel_length, memory_length])
     for index in range(channel_length):
         total_h[index] = estimate_channel(memory_length, gamma, channel_coefficients, noisy_est_var,
-                             fading, index, fading_taps_type)
+                                          fading, index, fading_taps_type)
     for i in range(memory_length):
         plt.plot(total_h[:, i], label=f'Tap {i}')
     plt.xlabel('Block Index')
--- python_code/plotters/plotter_main.py
+++ python_code/plotters/plotter_main.py
@@ -1,5 +1,4 @@
 from dir_definitions import WEIGHTS_DIR
-import os
 from python_code.plotters.plotter_utils import get_ser_plot, plot_all_curves_aggregated
 from python_code.trainers.META_LSTM.meta_lstm_trainer import MetaLSTMTrainer
 from python_code.trainers.META_VNET.metavnet_trainer import METAVNETTrainer
@@ -7,267 +6,135 @@ from python_code.trainers.LSTM.lstm_trainer import LSTMTrainer
 from python_code.trainers.VA.va_trainer import VATrainer
 from python_code.trainers.VNET.vnet_trainer import VNETTrainer
 from python_code.plotters.plotter_config import *
+import os
 
 
-
-def add_viterbi(all_curves, val_block_length, n_symbol, snr, channel_coefficients):
-    dec = VATrainer(val_SNR_start=snr,
-                    val_SNR_end=snr,
-                    train_SNR_start=snr,
-                    train_SNR_end=snr,
-                    val_SNR_step=2,
-                    train_SNR_step=2,
-                    val_block_length=val_block_length,
-                    train_block_length=val_block_length,
-                    noisy_est_var=0,
-                    fading_in_channel=False,
-                    fading_in_decoder=False,
-                    use_ecc=True,
-                    gamma_start=0.2,
-                    gamma_end=0.2,
-                    gamma_num=1,
-                    channel_type='ISI_AWGN',
-                    channel_coefficients='cost2100',
-                    subframes_in_frame=25,
-                    val_frames=VAL_FRAMES,
-                    eval_mode='by_word',
-                    n_symbols=n_symbol,
-                    fading_taps_type=1,
-                    self_supervised=False,
+def add_viterbi(all_curves,current_params):
+    dec = VATrainer(self_supervised=False,
                     online_meta=False,
                     weights_dir=os.path.join(WEIGHTS_DIR,
-                                             f'training_{val_block_length}_{n_symbol}_channel1'))
+                                             f'training_{HYPERPARAMS_DICT["val_block_length"]}_{HYPERPARAMS_DICT["n_symbol"]}_channel1'),
+                    **HYPERPARAMS_DICT)
     method_name = f'Viterbi - Full CSI'
-    ser = get_ser_plot(dec, run_over=run_over,
-                       method_name=method_name + f' - Block Length {val_block_length}, Error symbols {n_symbol}')
-    all_curves.append((ser, method_name, val_block_length, n_symbol))
+    ser = get_ser_plot(dec, run_over=run_over, method_name=method_name + current_params)
+    all_curves.append((ser, method_name, HYPERPARAMS_DICT['val_block_length'], HYPERPARAMS_DICT['n_symbol']))
 
 
-def add_joint_viterbinet(all_curves, val_block_length, n_symbol, snr, channel_coefficients):
-    dec = VNETTrainer(val_SNR_start=snr,
-                      val_SNR_end=snr,
-                      train_SNR_start=snr,
-                      train_SNR_end=snr,
-                      val_SNR_step=2,
-                      train_SNR_step=2,
-                      val_block_length=val_block_length,
-                      train_block_length=val_block_length,
-                      noisy_est_var=0,
-                      fading_in_channel=False,
-                      fading_in_decoder=False,
-                      use_ecc=True,
-                      gamma_start=0.2,
-                      gamma_end=0.2,
-                      gamma_num=1,
-                      channel_type='ISI_AWGN',
-                      channel_coefficients='cost2100',
-                      subframes_in_frame=25,
-                      val_frames=VAL_FRAMES,
-                      eval_mode='by_word',
-                      n_symbols=n_symbol,
-                      fading_taps_type=1,
-                      self_supervised=False,
+def add_joint_viterbinet(all_curves,current_params):
+    dec = VNETTrainer(self_supervised=False,
                       online_meta=False,
                       weights_dir=os.path.join(WEIGHTS_DIR,
-                                               f'training_{val_block_length}_{n_symbol}_channel1'))
+                                               f'training_{HYPERPARAMS_DICT["val_block_length"]}_{HYPERPARAMS_DICT["n_symbols"]}_channel1'),
+                      **HYPERPARAMS_DICT)
     method_name = f'Joint ViterbiNet'
-    ser = get_ser_plot(dec, run_over=run_over,
-                       method_name=method_name + f' - SNR {snr}, Block Length {val_block_length}, Error symbols {n_symbol}')
-    all_curves.append((ser, method_name, val_block_length, n_symbol))
+    ser = get_ser_plot(dec, run_over=run_over, method_name=method_name + current_params)
+    all_curves.append((ser, method_name, HYPERPARAMS_DICT['val_block_length'], HYPERPARAMS_DICT['n_symbols']))
 
 
-def add_joint_rnn(all_curves, val_block_length, n_symbol, snr, channel_coefficients):
-    dec = LSTMTrainer(val_SNR_start=snr,
-                      val_SNR_end=snr,
-                      train_SNR_start=snr,
-                      train_SNR_end=snr,
-                      val_SNR_step=2,
-                      train_SNR_step=2,
-                      val_block_length=val_block_length,
-                      train_block_length=val_block_length,
-                      noisy_est_var=0,
-                      fading_in_channel=False,
-                      fading_in_decoder=False,
-                      use_ecc=True,
-                      gamma_start=0.2,
-                      gamma_end=0.2,
-                      gamma_num=1,
-                      channel_type='ISI_AWGN',
-                      channel_coefficients='cost2100',
-                      subframes_in_frame=25,
-                      val_frames=VAL_FRAMES,
-                      eval_mode='by_word',
-                      n_symbols=n_symbol,
-                      fading_taps_type=1,
-                      self_supervised=False,
+def add_joint_rnn(all_curves,current_params):
+    dec = LSTMTrainer(self_supervised=False,
                       online_meta=False,
                       weights_dir=os.path.join(WEIGHTS_DIR,
-                                               f'rnn_training_{val_block_length}_{n_symbol}_channel1'))
+                                               f'rnn_training_{HYPERPARAMS_DICT["val_block_length"]}_{HYPERPARAMS_DICT["n_symbols"]}_channel1'),
+                      **HYPERPARAMS_DICT)
     method_name = f'JointRNN'
-    ser = get_ser_plot(dec, run_over=run_over,
-                       method_name=method_name + f' - SNR {snr}, Block Length {val_block_length}, Error symbols {n_symbol}')
-    all_curves.append((ser, method_name, val_block_length, n_symbol))
+    ser = get_ser_plot(dec, run_over=run_over, method_name=method_name + current_params)
+    all_curves.append((ser, method_name, HYPERPARAMS_DICT['val_block_length'], HYPERPARAMS_DICT['n_symbols']))
 
 
-def add_viterbinet(all_curves, val_block_length, n_symbol, snr, channel_coefficients):
-    dec = VNETTrainer(val_SNR_start=snr,
-                      val_SNR_end=snr,
-                      train_SNR_start=snr,
-                      train_SNR_end=snr,
-                      val_SNR_step=2,
-                      train_SNR_step=2,
-                      val_block_length=val_block_length,
-                      train_block_length=val_block_length,
-                      noisy_est_var=0,
-                      fading_in_channel=False,
-                      fading_in_decoder=False,
-                      use_ecc=True,
-                      gamma_start=0.2,
-                      gamma_end=0.2,
-                      gamma_num=1,
-                      channel_type='ISI_AWGN',
-                      channel_coefficients='cost2100',
-                      subframes_in_frame=25,
-                      val_frames=VAL_FRAMES,
-                      eval_mode='by_word',
-                      n_symbols=n_symbol,
-                      fading_taps_type=1,
-                      self_supervised=True,
+def add_viterbinet(all_curves,current_params):
+    dec = VNETTrainer(self_supervised=True,
                       online_meta=False,
                       weights_dir=os.path.join(WEIGHTS_DIR,
-                                               f'training_{val_block_length}_{n_symbol}_channel1'))
+                                               f'training_{HYPERPARAMS_DICT["val_block_length"]}_{HYPERPARAMS_DICT["n_symbols"]}_channel1'),
+                      **HYPERPARAMS_DICT)
     method_name = f'ViterbiNet'
-    ser = get_ser_plot(dec, run_over=run_over,
-                       method_name=method_name + f' - SNR {snr}, Block Length {val_block_length}, Error symbols {n_symbol}')
-    all_curves.append((ser, method_name, val_block_length, n_symbol))
+    ser = get_ser_plot(dec, run_over=run_over, method_name=method_name + current_params)
+    all_curves.append((ser, method_name, HYPERPARAMS_DICT['val_block_length'], HYPERPARAMS_DICT['n_symbols']))
 
 
-def add_rnn(all_curves, val_block_length, n_symbol, snr, channel_coefficients):
-    dec = LSTMTrainer(val_SNR_start=snr,
-                      val_SNR_end=snr,
-                      train_SNR_start=snr,
-                      train_SNR_end=snr,
-                      val_SNR_step=2,
-                      train_SNR_step=2,
-                      val_block_length=val_block_length,
-                      train_block_length=val_block_length,
-                      noisy_est_var=0,
-                      fading_in_channel=False,
-                      fading_in_decoder=False,
-                      use_ecc=True,
-                      gamma_start=0.2,
-                      gamma_end=0.2,
-                      gamma_num=1,
-                      channel_type='ISI_AWGN',
-                      channel_coefficients='cost2100',
-                      subframes_in_frame=25,
-                      val_frames=VAL_FRAMES,
-                      eval_mode='by_word',
-                      n_symbols=n_symbol,
-                      fading_taps_type=1,
-                      self_supervised=True,
+def add_rnn(all_curves,current_params):
+    dec = LSTMTrainer(self_supervised=True,
                       online_meta=False,
                       weights_dir=os.path.join(WEIGHTS_DIR,
-                                               f'rnn_training_{val_block_length}_{n_symbol}_channel1'))
+                                               f'rnn_training_{HYPERPARAMS_DICT["val_block_length"]}_{HYPERPARAMS_DICT["n_symbols"]}_channel1'),
+                      **HYPERPARAMS_DICT)
     method_name = f'LSTM'
-    ser = get_ser_plot(dec, run_over=run_over,
-                       method_name=method_name + f' - SNR {snr}, Block Length {val_block_length}, Error symbols {n_symbol}')
-    all_curves.append((ser, method_name, val_block_length, n_symbol))
+    ser = get_ser_plot(dec, run_over=run_over, method_name=method_name + current_params)
+    all_curves.append((ser, method_name, HYPERPARAMS_DICT['val_block_length'], HYPERPARAMS_DICT['n_symbols']))
 
 
-def add_onlinemetaviterbinet(all_curves, val_block_length, n_symbol, snr, channel_coefficients):
-    dec = METAVNETTrainer(val_SNR_start=snr,
-                          val_SNR_end=snr,
-                          train_SNR_start=snr,
-                          train_SNR_end=snr,
-                          val_SNR_step=2,
-                          train_SNR_step=2,
-                          val_block_length=val_block_length,
-                          train_block_length=val_block_length,
-                          noisy_est_var=0,
-                          fading_in_channel=False,
-                          fading_in_decoder=False,
-                          use_ecc=True,
-                          gamma_start=0.2,
-                          gamma_end=0.2,
-                          gamma_num=1,
-                          channel_type='ISI_AWGN',
-                          channel_coefficients='cost2100',
-                          subframes_in_frame=25,
-                          val_frames=VAL_FRAMES,
-                          eval_mode='by_word',
-                          n_symbols=n_symbol,
-                          fading_taps_type=1,
-                          self_supervised_iterations=200,
-                          self_supervised=True,
-                          ser_thresh=0.02,
+def add_onlinemetaviterbinet(all_curves,current_params):
+    dec = METAVNETTrainer(self_supervised=True,
                           online_meta=True,
-                          buffer_empty=True,
-                          weights_init='last_frame',
                           weights_dir=os.path.join(WEIGHTS_DIR,
-                                                   f'meta_training_{val_block_length}_{n_symbol}_channel1'))
+                                                   f'meta_training_{HYPERPARAMS_DICT["val_block_length"]}_{HYPERPARAMS_DICT["n_symbols"]}_channel1'),
+                          **HYPERPARAMS_DICT)
     method_name = f'OnlineMetaViterbiNet'
-    ser = get_ser_plot(dec, run_over=run_over,
-                       method_name=method_name + f' - SNR {snr}, Block Length {val_block_length}, Error symbols {n_symbol}')
-    all_curves.append((ser, method_name, val_block_length, n_symbol))
+    ser = get_ser_plot(dec, run_over=run_over, method_name=method_name + current_params)
+    all_curves.append((ser, method_name, HYPERPARAMS_DICT['val_block_length'], HYPERPARAMS_DICT['n_symbols']))
 
 
-def add_online_metarnn(all_curves, val_block_length, n_symbol, snr, channel_coefficients):
-    dec = MetaLSTMTrainer(val_SNR_start=snr,
-                          val_SNR_end=snr,
-                          train_SNR_start=snr,
-                          train_SNR_end=snr,
-                          val_SNR_step=2,
-                          train_SNR_step=2,
-                          val_block_length=val_block_length,
-                          train_block_length=val_block_length,
-                          noisy_est_var=0,
-                          fading_in_channel=False,
-                          fading_in_decoder=False,
-                          use_ecc=True,
-                          gamma_start=0.2,
-                          gamma_end=0.2,
-                          gamma_num=1,
-                          channel_type='ISI_AWGN',
-                          channel_coefficients='cost2100',
-                          subframes_in_frame=25,
-                          val_frames=VAL_FRAMES,
-                          eval_mode='by_word',
-                          n_symbols=n_symbol,
-                          fading_taps_type=1,
-                          self_supervised_iterations=200,
-                          self_supervised=True,
-                          ser_thresh=0.02,
-                          online_meta=False,
-                          buffer_empty=True,
-                          weights_init='last_frame',
+def add_online_metarnn(all_curves,current_params):
+    dec = MetaLSTMTrainer(self_supervised=True,
+                          online_meta=True,
                           weights_dir=os.path.join(WEIGHTS_DIR,
-                                                   f'rnn_meta_training_{val_block_length}_{n_symbol}_channel1'))
+                                                   f'rnn_meta_training_{HYPERPARAMS_DICT["val_block_length"]}_{HYPERPARAMS_DICT["n_symbols"]}_channel1'),
+                          **HYPERPARAMS_DICT)
     method_name = f'OnlineRNN'
-    ser = get_ser_plot(dec, run_over=run_over,
-                       method_name=method_name + f' - SNR {snr}, Block Length {val_block_length}, Error symbols {n_symbol}')
-    all_curves.append((ser, method_name, val_block_length, n_symbol))
+    ser = get_ser_plot(dec, run_over=run_over, method_name=method_name + current_params)
+    all_curves.append((ser, method_name, HYPERPARAMS_DICT['val_block_length'], HYPERPARAMS_DICT['n_symbols']))
 
 
-VAL_FRAMES = 20
+HYPERPARAMS_DICT = {'val_SNR_step': 2,
+                    'train_SNR_step': 2,
+                    'noisy_est_var': 0,
+                    'fading_taps_type': 2,
+                    'fading_in_decoder': True,
+                    'use_ecc': True,
+                    'gamma_start': 0.2,
+                    'gamma_end': 0.2,
+                    'gamma_num': 1,
+                    'channel_type': 'ISI_AWGN',
+                    'val_frames': 12,
+                    'subframes_in_frame': 25,
+                    'eval_mode': 'by_word',
+                    'self_supervised_iterations': 200,
+                    'ser_thresh': 0.02,
+                    'buffer_empty': True,
+                    'weights_init': 'last_frame',
+                    }
 
 if __name__ == '__main__':
     run_over = False
-    parameters = [(7, 120)]  # ,(8, 120)
+    parameters = [(12, 120)]  # ,(8, 120)
     n_symbol = 2
-    channel_coefficients = 'cost2100'  # 'time_decay','cost2100'
+    channel_coefficients = 'time_decay'  # 'time_decay','cost2100'
 
     for snr, val_block_length in parameters:
         all_curves = []
         print(snr, val_block_length, n_symbol)
-        add_joint_viterbinet(all_curves, val_block_length, n_symbol, snr, channel_coefficients)
-        add_joint_rnn(all_curves, val_block_length, n_symbol, snr, channel_coefficients)
-        add_viterbinet(all_curves, val_block_length, n_symbol, snr, channel_coefficients)
-        add_rnn(all_curves, val_block_length, n_symbol, snr, channel_coefficients)
-        add_onlinemetaviterbinet(all_curves, val_block_length, n_symbol, snr, channel_coefficients)
-        add_online_metarnn(all_curves, val_block_length, n_symbol, snr, channel_coefficients)
-        add_viterbi(all_curves, val_block_length, n_symbol, snr, channel_coefficients)
+
+        HYPERPARAMS_DICT['n_symbols'] = n_symbol
+        HYPERPARAMS_DICT['val_SNR_start'] = snr
+        HYPERPARAMS_DICT['val_SNR_end'] = snr
+        HYPERPARAMS_DICT['train_SNR_start'] = snr
+        HYPERPARAMS_DICT['train_SNR_end'] = snr
+        HYPERPARAMS_DICT['val_block_length'] = val_block_length
+        HYPERPARAMS_DICT['train_block_length'] = val_block_length
+        HYPERPARAMS_DICT['fading_in_channel'] = True if channel_coefficients == 'time_decay' else False
+        HYPERPARAMS_DICT['channel_coefficients'] = channel_coefficients
+
+        current_params = HYPERPARAMS_DICT['channel_coefficients'] + '_' + str(HYPERPARAMS_DICT['val_SNR_start']) + '_' + \
+                         str(HYPERPARAMS_DICT['val_block_length']) + '_' + str(HYPERPARAMS_DICT['n_symbols'])
+
+        add_joint_viterbinet(all_curves,current_params)
+        # add_joint_rnn(all_curves,current_params)
+        add_viterbinet(all_curves,current_params)
+        # add_rnn(all_curves,current_params)
+        add_onlinemetaviterbinet(all_curves,current_params)
+        # add_online_metarnn(all_curves,current_params)
+        # add_viterbi(all_curves,current_params)
 
         plot_all_curves_aggregated(all_curves, val_block_length, n_symbol, snr)
-        # plot_all_curves(all_curves, val_block_length, n_symbol)
     # plot_schematic(all_curves, snr_values)
